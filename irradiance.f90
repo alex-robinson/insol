c0b36d225783236ddda59b4866b24a1427b641bb
Alexander Robinson <robinson@ucm.es>
2020-07-20 22:34:52 +0300

    Bug fixes in irradiance code- dni calculations seem to be working now.

--- irradiance.f90
+++ irradiance.f90
@@ -24,6 +24,10 @@ module irradiance
     real(wp), parameter :: degrees_to_radians = real(pi / 180._dp,wp)  ! Conversion factor between radians and degrees
     real(wp), parameter :: radians_to_degrees = real(180._dp / pi,wp)  ! Conversion factor between degrees and radians
     
+    private 
+    public :: sp, dp, wp, MISSING_VALUE, MV, pi, degrees_to_radians, radians_to_degrees
+    public :: calc_dni_point
+    public :: calc_zenith_angle
 
 contains 
 
@@ -50,9 +54,9 @@ contains
 
         real(wp) :: etr 
         real(wp) :: dec 
-        real(wp) :: eqt 
-        real(wp) :: hour_angle 
+        real(wp) :: eqt  
         real(wp) :: zenith_angle
+        real(wp) :: cos_zenith_angle
         real(wp) :: am, kt, kn, knc
         real(wp) :: a, b, c 
 
@@ -63,8 +67,11 @@ contains
         day_of_year  = int((hour_of_year - 1) / 24) + 1
         latitude     = lat
         longitude    = lon
-        day_angle    = (2.0*pi) * min( (day_of_year - 1) / 365.0_wp, 1.0)
-        !pressure     = pres
+        day_angle    = (2.0*pi) * (day_of_year - 1) / 365.0_wp
+
+        ! Calculate zenith angle and its cosine
+        call calc_zenith_angle(zenith_angle,hour_of_year,latitude,longitude)
+        cos_zenith_angle = cos(zenith_angle*degrees_to_radians) 
 
         ! Assume standard pressure for now 
         pressure     = 1013.25_wp 
@@ -72,31 +79,28 @@ contains
         ! Assume that all input data is in UTC time zone 
         time_zone = 0.0_wp 
 
-        ! Calculate time zone as a function of longitude 
-
-
+        ! Normal insolation at the top of the atmosphere
         etr = S0 * (1.00011 + 0.034221 * cos(day_angle) + 0.00128 * sin(day_angle) + &
               0.000719 * cos(2.0 * day_angle) + 0.000077 * sin(2.0 * day_angle))
-        dec = (0.006918 - 0.399912 * cos(day_angle) + 0.070257 * sin(day_angle) -  &
-              0.006758 * cos(2.0 * day_angle) + 0.000907 * sin(2.0 * day_angle) -  &
-              0.002697 * cos(3.0 * day_angle) + 0.00148 * sin(3.0 * day_angle)) * radians_to_degrees
-        eqt = (0.000075 + 0.001868 * cos(day_angle) - 0.032077 * sin(day_angle) -  &
-              0.014615 * cos(2 * day_angle) - 0.040849 * sin(2.0 * day_angle)) * (229.18)
-        hour_angle = 15 * (hour_of_year - 12 - 0.5 + eqt / 60 + ((longitude - time_zone * 15) * 4) / 60)
-        zenith_angle = acos(cos(dec*degrees_to_radians) * cos(latitude*degrees_to_radians) * cos(hour_angle*degrees_to_radians) +  &
-              sin(dec*degrees_to_radians) * sin(latitude*degrees_to_radians)) * radians_to_degrees
+        
+        !am =  (1.0/cos_zenith_angle)
         if (zenith_angle < 80) then 
-            am = 1.0 / (cos(zenith_angle*degrees_to_radians) + 0.15 / pow(93.885 - zenith_angle, 1.253)) * (pressure / 1013.25)
+            am = 1.0 / (cos_zenith_angle + 0.15 / pow(93.885 - zenith_angle, 1.253)) * (pressure / 1013.25)
         else
             am = 0.0
         end if
 
         if (am > 0) then 
-            kt = ghi / (cos(zenith_angle*degrees_to_radians) * etr)
+            kt = ghi / (cos_zenith_angle * etr)
         else
             kt = 0.0
         end if
         
+        if (kt .gt. 1.0) then 
+            write(*,*) "kt > 1 !" 
+            write(*,*) "kt = ", kt, ghi, etr, cos_zenith_angle, zenith_angle
+            stop 
+        end if
 
         a = 0.0
         if (kt > 0) then 
@@ -141,10 +145,70 @@ contains
             dni = 0.0_wp 
         end if
 
+        !write(*,*) "testing: ", hour_of_year, zenith_angle, am, kt, a, b, c, kn, knc, dni
+        !stop 
+
         return 
 
     end subroutine calc_dni_point
 
+
+    subroutine calc_zenith_angle(zenith_angle,hour,lat,lon)
+
+        implicit none 
+
+        real(wp), intent(OUT) :: zenith_angle 
+        real(wp), intent(IN)  :: hour
+        real(wp), intent(IN)  :: lat
+        real(wp), intent(IN)  :: lon
+        
+        ! Local variables 
+        real(wp) :: hour_of_year
+        integer  :: day_of_year
+        real(wp) :: latitude
+        real(wp) :: longitude
+        real(wp) :: day_angle
+        real(wp) :: time_zone 
+         
+        real(wp) :: dec 
+        real(wp) :: eqt 
+        real(wp) :: hour_angle 
+        real(wp) :: cos_zenith_angle 
+
+        real(wp), parameter :: S0 = 1370.0_wp 
+
+        ! Assign input arguments to local variables 
+        hour_of_year = hour
+        day_of_year  = int((hour_of_year - 1) / 24) + 1
+        latitude     = lat
+        longitude    = lon
+        !day_angle    = (2.0*pi) * min( (day_of_year - 1) / 365.0_wp, 1.0)
+        day_angle    = (2.0*pi) * (day_of_year - 1) / 365.0_wp
+
+        ! Assume that all input data is in UTC time zone 
+        time_zone = 0.0_wp 
+
+        dec = (0.006918 - 0.399912 * cos(day_angle) + 0.070257 * sin(day_angle) -   &
+              0.006758 * cos(2 * day_angle) + 0.000907 * sin(2 * day_angle) -       &
+              0.002697 * cos(3 * day_angle) + 0.00148 * sin(3 * day_angle)) * (180.0 / 3.14159)
+        eqt = (0.000075 + 0.001868 * cos(day_angle) - 0.032077 * sin(day_angle) -   &
+              0.014615 * cos(2 * day_angle) - 0.040849 * sin(2 * day_angle)) * (229.18)
+        
+        hour_angle = 15 * (hour_of_year - 12 - 0.5 + eqt / 60 + ((longitude - time_zone * 15) * 4) / 60)
+        
+        ! Calculate cosine of zenith angle 
+        cos_zenith_angle =  cos(dec*degrees_to_radians)  * &
+                            cos(latitude*degrees_to_radians) * &
+                            cos(hour_angle*degrees_to_radians) &
+                          + sin(dec*degrees_to_radians) * &
+                            sin(latitude*degrees_to_radians)
+
+        ! Finally, calculate the zenith angle [degrees]
+        zenith_angle = acos(cos_zenith_angle) * radians_to_degrees
+
+        return 
+
+    end subroutine calc_zenith_angle
 
     function pow(var,power) result(var_pow)
 
--- test_dni.f90
+++ test_dni.f90
@@ -7,30 +7,79 @@ program test_dni
     use irradiance  
     use ncio 
 
-    integer          :: nday, day
-    double precision :: lat, time_bp, insol
-
-    integer  :: nt 
+    integer  :: i, j, k, nx, ny, nt 
+    real(wp), allocatable :: lon(:) 
+    real(wp), allocatable :: lat(:) 
     real(wp), allocatable :: time(:) 
-    real(wp), allocatable :: ghi(:) 
+    real(wp), allocatable :: ghi(:,:,:) 
+    real(wp), allocatable :: zenith_angle(:,:,:) 
+    real(wp), allocatable :: dni(:,:,:) 
 
     character(len=512) :: filename_in 
     character(len=512) :: filename_out  
 
-    filename_in = "data/era5/download_2019.nc" 
-
+    filename_in  = "data/era5/southspain/era5_southspain_2019.nc" 
+    filename_out = "data/era5/southspain/era5_southspain_2019_dni.nc"
+    
+    nx = nc_size(filename_in,"longitude")
+    ny = nc_size(filename_in,"latitude")
     nt = nc_size(filename_in,"time") 
 
+    allocate(lon(nx))
+    allocate(lat(ny))
     allocate(time(nt))
-    allocate(ghi(nt)) 
+    allocate(ghi(nx,ny,nt)) 
+    allocate(zenith_angle(nx,ny,nt)) 
+    allocate(dni(nx,ny,nt))
 
+    call nc_read(filename_in,"longitude",lon)
+    call nc_read(filename_in,"latitude",lat)
     call nc_read(filename_in,"time",time)
-    call nc_read(filename_in,"ssrd",ghi,start=[2,2,1],count=[1,1,nt])
+    call nc_read(filename_in,"ssrd",ghi)
 
     time = time - minval(time) 
-    ghi  = ghi / 86400.0_wp 
+    ghi  = ghi / 3600.0_wp 
+
     
+    write(*,*) "range(lon):  ", minval(lon),  maxval(lon)
+    write(*,*) "range(lat):  ", minval(lat),  maxval(lat)
     write(*,*) "range(time): ", minval(time), maxval(time)
-    write(*,*) "range(ghi):  ", minval(ghi), maxval(ghi)
+    write(*,*) "range(ghi):  ", minval(ghi),  maxval(ghi)
+
+    zenith_angle = 0.0_wp 
+    dni = 0.0_wp 
+
+    i = 2 
+    j = 2 
+
+        do k = 1, nt 
+            call calc_zenith_angle(zenith_angle(i,j,k),time(k),lat(j),lon(i))
+            call calc_dni_point(dni(i,j,k),ghi(i,j,k),time(k),lat(j),lon(i))
+        end do
+
+
+    ! do i = 1, nx 
+    ! do j = 1, ny 
+
+    !     do k = 1, nt 
+    !         call calc_dni_point(dni(i,j,k),ghi(i,j,k),time(k),lat(j),lon(i))
+    !     end do
+
+    ! end do 
+    ! end do 
+
+
+    ! Write to output file 
+
+    call nc_create(filename_out)
+    call nc_write_dim(filename_out,"longitude",x=lon)
+    call nc_write_dim(filename_out,"latitude",x=lat)
+    call nc_write_dim(filename_out,"time",x=time)
+    
+    
+    call nc_write(filename_out,"zenith_angle",  zenith_angle,   dim1="longitude",dim2="latitude",dim3="time")
+    call nc_write(filename_out,"ghi",           ghi,            dim1="longitude",dim2="latitude",dim3="time")
+    call nc_write(filename_out,"dni",           dni,            dim1="longitude",dim2="latitude",dim3="time")
+    
 
 end program test_dni
